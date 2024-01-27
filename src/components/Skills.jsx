a6713904d629fb0c0f1fb7713954c20596e03765
Nathan <natenderitu@gmail.com>
2024-01-27 09:02:41 +0300

    fix: change image urls for skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,31 +1,43 @@
+import html from '../assets/icons/html5-02-svgrepo-com.svg';
+import css from '../assets/icons/css3-02-svgrepo-com.svg';
+import laravel from '../assets/icons/laravel-svgrepo-com.svg';
+import mysql from '../assets/icons/mysql-svgrepo-com.svg';
+import postman from '../assets/icons/postman-svgrepo-com.svg';
+import python from '../assets/icons/python-svgrepo-com.svg';
+import pytorch from '../assets/icons/pytorch-svgrepo-com.svg'
+import tailwind from '../assets/icons/tailwind-svgrepo-com.svg'
+import tensorflow from '../assets/icons/tensorflow-svgrepo-com.svg'
+
+
 export default function Skills() {
     return (
         <section id="skills" className="section-wrapper skill">
 		<h2>Skills</h2>
 		<div className="skills-container">
 			<div className="card">
-				<img src="../assets/icons/html5-02-svgrepo-com.svg" alt="HTML"/>
+				<img src={html} alt="HTML"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/css3-02-svgrepo-com.svg" alt="CSS"/>
+				<img src={css} alt="CSS"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/mysql-svgrepo-com.svg" alt="MySQL"/>
+				<img src={mysql} alt="MySQL"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/laravel-svgrepo-com.svg" alt="Laravel"/>
+				<img src={laravel} alt="Laravel"/>
+			</div>
+		
+			<div className="card">
+				<img src={postman} alt="Postman"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/postman-svgrepo-com.svg" alt="Postman"/>
+				<img src={python} alt="Python"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/python-svgrepo-com.svg" alt="Python"/>
+				<img src={tensorflow} alt="Tensorflow"/>
 			</div>
 			<div className="card">
-				<img src="../assets/icons/tensorflow-svgrepo-com.svg" alt="Tensorflow"/>
-			</div>
-			<div className="card">
-				<img src="../assets/icons/pytorch-svgrepo-com.svg" alt="Pytorch"/>
+				<img src={pytorch} alt="Pytorch"/>
 			</div>
 		</div>
 	</section>   
