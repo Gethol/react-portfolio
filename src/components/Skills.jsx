8887c03b8b5874dc28c50be1beeaa69f448ac6f5
Nathan <natenderitu@gmail.com>
2024-01-27 07:36:52 +0300

    chore: adjust links to the svg icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,28 +4,28 @@ export default function Skills() {
 		<h2>Skills</h2>
 		<div className="skills-container">
 			<div className="card">
-				<img src="assets/icons/html5-02-svgrepo-com.svg" alt="HTML"/>
+				<img src="../assets/icons/html5-02-svgrepo-com.svg" alt="HTML"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/css3-02-svgrepo-com.svg" alt="CSS"/>
+				<img src="../assets/icons/css3-02-svgrepo-com.svg" alt="CSS"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/mysql-svgrepo-com.svg" alt="MySQL"/>
+				<img src="../assets/icons/mysql-svgrepo-com.svg" alt="MySQL"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/laravel-svgrepo-com.svg" alt="Laravel"/>
+				<img src="../assets/icons/laravel-svgrepo-com.svg" alt="Laravel"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/postman-svgrepo-com.svg" alt="Postman"/>
+				<img src="../assets/icons/postman-svgrepo-com.svg" alt="Postman"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/python-svgrepo-com.svg" alt="Python"/>
+				<img src="../assets/icons/python-svgrepo-com.svg" alt="Python"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/tensorflow-svgrepo-com.svg" alt="Tensorflow"/>
+				<img src="../assets/icons/tensorflow-svgrepo-com.svg" alt="Tensorflow"/>
 			</div>
 			<div className="card">
-				<img src="assets/icons/pytorch-svgrepo-com.svg" alt="Pytorch"/>
+				<img src="../assets/icons/pytorch-svgrepo-com.svg" alt="Pytorch"/>
 			</div>
 		</div>
 	</section>   
