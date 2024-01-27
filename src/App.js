4d706ba8ab090cf836bbc3243a9a6512ce57fe48
Nathan <natenderitu@gmail.com>
2024-01-27 06:52:59 +0300

    fix: change hero import link in main app file

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import './components/Navigation.jsx'
 import Navigation from './components/Navigation';
-import Hero from './components/AboutUs.jsx'
+import Hero from './components/Hero.jsx'
 import AboutUs from './components/AboutUs.jsx';
 
 function Navbar(){
