9d65f24119eac9bc7478fcc607cae8b44f54e918
Nathan <natenderitu@gmail.com>
2024-01-27 08:51:49 +0300

    fix: change image urls in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,27 @@
+import linkedin from '../assets/icons/linkedin-svgrepo-com.svg';
+import github from "../assets/icons/github-142-svgrepo-com.svg"
+import photo from "../assets/public/profile_photo.jpg"
+
 export default function Hero(){
     return(
         <section className="banner">
 
-		<div className="intro">
+		<div className="intro">/assets/icons/pytorch-svgrepo-com.svg
 			<h1>Welcome to My Portfolio!</h1>
 			<p>I'm Nathan Nderitu, a software developer.</p>
 			<div className="btn-wrapper">
 
 				<a href="www.linkedin.com/in/nathan-nderitu-98aa36247">
-					<img src="assets/icons/linkedin-svgrepo-com.svg" alt="linkedin"/>
+					<img src={linkedin} alt="linkedin"/>
 				</a>
 
 				<a href="https://github.com/Gethol">
-					<img src="assets/icons/github-142-svgrepo-com.svg" alt="github" />
+					<img src={github} alt="github" />
 				</a>
 			</div>
 		</div>
 
-		<img className="img-container" src="assets/public/profile_photo.jpg" alt="Nathan Nderitu" />
+		<img className="img-container" src={photo} alt="Nathan Nderitu" />
 
 
 	</section> 
